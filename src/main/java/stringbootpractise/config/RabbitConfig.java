b5e6b534a9e39b1784edf65fc2cdd063a6ec938b
Mazexal <1023520044@qq.com>
2017-07-27 13:41:23 +0800

    myhabits

---
+++ src/main/java/stringbootpractise/config/RabbitConfig.java
@@ -0,0 +1,19 @@
+package stringbootpractise.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Queue;
+
+/**
+ * Created by Mazexal on 2017/7/27.
+ */
+
+@Configuration
+public class RabbitConfig {
+//    @Bean
+//    public Queue Queue() {
+//        return new Queue("hello");
+//    }
+
+}
