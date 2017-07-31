8867f9a176cefadf8c22ec6c64a3e7d49766ad8c
Mazexal <1023520044@qq.com>
2017-07-31 21:10:53 +0800

    druid

--- src/main/java/stringbootpractise/Druid/DruidConfiguration.java
+++ src/main/java/stringbootpractise/Druid/DruidConfiguration.java
@@ -1,6 +1,8 @@
 package stringbootpractise.Druid;
 
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,4 +29,15 @@ public class DruidConfiguration {
         servletRegistrationBean.addInitParameter("resetEnable", "false");
         return servletRegistrationBean;
     }
+
+    @Bean
+    public FilterRegistrationBean druidStatFilter2(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid2/*");
+        return filterRegistrationBean;
+    }
+
 }
