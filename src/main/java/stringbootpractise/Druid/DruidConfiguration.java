70750c0ce3e4f5bf90797e5b659ad6a1fb215f3c
Mazexal <1023520044@qq.com>
2017-07-31 21:09:45 +0800

    druid

---
+++ src/main/java/stringbootpractise/Druid/DruidConfiguration.java
@@ -0,0 +1,30 @@
+package stringbootpractise.Druid;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 10235 on 2017/7/31.
+ */
+@Configuration
+public class DruidConfiguration {
+    @Bean
+    public ServletRegistrationBean DruidStatViewServle2() {
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new
+                StatViewServlet(), "/druid2/*");
+        //添加初始化参数：initParams
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny", "192.168.1.73");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername", "admin2");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+}
